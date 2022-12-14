1301d3aaf66f90c940579469a5c279b1fa007b43
MrS-E <simeon.stix@gmx.ch>
2022-12-14 16:33:02 +0100

    login -> few updates register -> new

--- server/index.js
+++ server/index.js
@@ -17,20 +17,46 @@ const db = mysql.createConnection({ //DB Connection
 
 app.get("/login", (req, res)=>{
     const mail = req.body.mail;
-    const password = req.body.password; //TODO hash later
-    db.query("", [mail, password], (err, result) => {
+    const password = req.body.password; //TODO (MrS-E) hash later
+    db.query("", [mail, password], //TODO (Joscupe) select user which has the password
+        (err, result) => {
         if(err){
             console.log(err)
+            res.send({login:false, error: err})
         }else{
             if(result.length===1){
-                res.send(true);
+                if(password===result[0].UsPasswd){
+                    result.send({login:true, error: null})
+                }else{
+                    result.send({login:false, error: "wrong password"})
+                }
             }else{
-                res.send(false);
+                res.send({login:false, error: "user not found"});
             }
         }
-    }) //TODO (Joscupe) select user which has the password
-})
+    });
+});
+
+app.post("/register", (req, res) => {
+    const mail = req.body.mail;
+    const salutation = req.body.salutation;
+    const first = req.body.firstName;
+    const last = req.body.lastName;
+    const password = req.body.password; //TODO (MrS-E) hash
+
+    db.query("", [mail, salutation,first,last,password], //TODO (Joscupe) insert user
+        (err) => {
+            if (err) {
+                console.log(err);
+                res.send({register:false, error:err})
+            } else {
+                res.send({register:true});
+            }
+        }
+    );
+});
+
 
 app.listen(5000, () => {
-    console.log("Server started");
+    console.log("Server started at port 5000");
 });
