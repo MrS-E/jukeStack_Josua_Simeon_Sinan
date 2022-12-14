dd00348c7a749e077e98c7b755c2d908ec5e074e
MrS-E <simeon.stix@gmx.ch>
2022-12-14 18:09:05 +0100

    the return of lendings the auto return of sounds the manual return of sounds

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 import { key } from "./db_key"; //keyfile with has an exported const with db key (file is in .gitignore)
-import sha256 from "crypto-js/sha256"
+import sha256 from "crypto-js/sha256";
 const express = require("express");
-const app = express();
 const mysql = require("mysql");
+var schedule = require('node-schedule');
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
+const app = express();
 app.use(cors());
 app.use(bodyParser.json()); //to manage the parsing of the body from react app
 
@@ -101,11 +102,11 @@ app.post("/lent", (req, res)=>{
 
 app.get("/lendings", (req, res)=>{
     const user = req.body.mail;
-    db.query("", //TODO (Joscupe) select all lent songs from user
+    db.query("", //TODO (Joscupe) select all lent songs from user which have no end date (if return with deletion additions are needed by (MrS-E)).
         [user],
         (err, result)=>{
         if(err){
-            console.log(err);
+            console.log("lendings:",err);
             res.send({lending:undefined, error:err});
         }else{
             if(result.length>0) {
@@ -116,6 +117,41 @@ app.get("/lendings", (req, res)=>{
         }
         }
     );
+});
+
+app.put("/return", (req, res)=>{
+    const user = req.body.mail;
+    const NFToken = req.body.NFtoken;
+    const LentID = req.body.songId;
+    db.query("", //TODO (Joscupe) update of lending with end time. Maybe deletion -> (MrS-E) edit of function
+        [user,NFToken,LentID],
+        (err)=>{
+        if(err){
+            console.log("return:",err);
+            res.send({return:false, error:err})
+        }else{
+            res.send({return:true, song: NFToken, user: user});
+        }
+        });
+});
+
+schedule.scheduleJob('0 0 * * *', ()=>{ //runs every 24h at 0:0
+    db.query("", //TODO (Joscupe) select all lendings which have expired or which have no end date (would be nice if start date = 5 days in the past (if not additions are needed by (MrS-E)))
+        (err, result)=>{
+            if(err){
+                console.log(err);
+            }else{
+                result.forEach((d)=>{
+                    db.query("", //TODO (Joscupe) update lending with end date -> if deletion additions are needed here by (MrS-E & Joscupe)
+                        [d.LenId],
+                        (err)=>{
+                            if(err){
+                                console.log(err);
+                            }
+                        });
+                });
+            }
+        });
 });
 
 app.listen(8080, () => {
