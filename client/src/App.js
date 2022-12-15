b4f955cd80672461e7cf0d721609d76bc8f87f87
MrS-E <simeon.stix@gmx.ch>
2022-12-15 09:56:36 +0100

    connect pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,20 @@ import LOGIN from "./login.js";
 
 function App() {
   const [user, changeUser] = useState("");
-  const [page, changepage] = useState("");
-  if (page === "login.js") {
+  const [page, changepage] = useState("login");
+  if (page === "login") {
     return (
       <div className="App">
         <LOGIN changepage={changepage}></LOGIN>
       </div>
     );
+  }else {
+    return (
+        <div className="App">
+         hey
+        </div>
+    );
   }
-  return (
-    <div className="App">
-      <LOGIN changeUser={changeUser}></LOGIN>
-    </div>
-  );
 }
 
 export default App;
